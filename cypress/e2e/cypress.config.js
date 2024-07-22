028a11fced235502c015f75ede01009ef19e33c3
DevonAGreene <greened8@mail.uc.edu>
2024-07-22 00:45:16 -0400

    Part 5

---
+++ cypress/e2e/cypress.config.js
@@ -0,0 +1,12 @@
+// cypress.config.js
+
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000', // Adjust to match your app's URL
+  },
+});
